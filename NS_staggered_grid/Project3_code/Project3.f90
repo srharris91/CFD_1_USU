2fbbd5f64bb5061a76172b40e88a027d6725f5eb
Shaun_Harris <shaun.r.harris@gmail.com>
2016-03-29 11:24:02 -0600

    Added python output, and almost finished momentum equations

--- NS_staggered_grid/Project3_code/Project3.f90
+++ NS_staggered_grid/Project3_code/Project3.f90
@@ -8,98 +8,6 @@
 ! x and y number of cells plus 2 (to account for boundary nodes)
 #define max_x2p 42
 #define max_y2p 42
-MODULE types
-    !purpose: define data type struct
-    IMPLICIT NONE
-    TYPE::dat
-        REAL::x,y,u,v,AP,AE,AN,AS,AW
-        INTEGER::n
-    END TYPE dat
-CONTAINS
-    SUBROUTINE set_xy (strct,dx,dy,nx,ny)
-        REAL,INTENT(IN)     ::  dx,dy
-        INTEGER,INTENT(IN)  ::  nx,ny   ! size of strct in x and y directions 
-        TYPE(dat),DIMENSION(0:nx-1,0:nx-1),INTENT(INOUT)::strct ! data contained from 0:nx-1 where cells 0 and nx-1 are boundary nodes (cell volume approaches 0 on boundary nodes)
-        INTEGER ::  i,j,n   ! for do loops and n is counter for cell number
-        REAL    ::  xi,yi   ! x and y values for each cell
-        n=1                     ! cell number 1
-        DO i=1,ny-2             ! 1 to ny-2 for boundary nodes (we only are iterating through the middle values)
-            yi = i*dy - dy/2.   ! y coordinate
-            DO j=1,nx-2
-                xi = j*dx - dx/2.       ! x coordinate
-                strct(i,j)%n = n        ! input n node
-                strct(i,j)%x = xi       ! x coordinate to strct
-                strct(i,j)%y = yi       ! y coordinate to strct
-                strct(i,j)%phi = 0.     ! phi value initialized guess
-                strct(i,j)%phi_new = 0. ! phi value initialized guess for next iteration
-                n=n+1                   ! count cell numbers up one
-            END DO
-        END DO
-    ! left boundary
-    strct(:,0)%x = 0.
-    strct(0,0)%y = 0.
-    strct(1:max_x,0)%y = RESHAPE((/ (i*dy - dy/2. ,i=1,max_x) /),(/ max_x /))
-    strct(max_xp,0)%y = 1.
-    ! top boundary
-    strct(0,0)%x = 0.
-    strct(0,1:max_y)%x = RESHAPE((/ (i*dx - dx/2. ,i=1,max_y) /),(/ max_y /))
-    strct(0,max_yp)%x = 1.
-    strct(0,:)%y = 0.
-    ! right boundary
-    strct(:,max_yp)%x = 1.
-    strct(:,max_yp)%y = strct(:,0)%y
-    ! bottom boundary
-    strct(max_xp,:)%x = strct(0,:)%x
-    strct(max_xp,:)%y = 1.
-    END SUBROUTINE set_xy
-    SUBROUTINE umom(strct,dx,dy,nx,ny)
-        REAL,INTENT(IN)     ::  dx,dy
-        INTEGER,INTENT(IN)  ::  nx,ny   ! size of strct in x and y directions 
-        TYPE(dat),DIMENSION(0:nx-1,0:nx-1),INTENT(INOUT)::strct ! data contained from 0:nx-1 where cells 0 and nx-1 are boundary nodes (cell volume approaches 0 on boundary nodes)
-        REAL    ::  mu = 0.01
-        REAL    ::  rho= 1.
-        REAL    ::  mdot ! temporary value for mass flow values
-        INTEGER ::  i,j !loop iterators
-        DO i=2,max_x
-            DO j=1,max_y
-                mdot            =   rho*strct(i+1,j  )%u*dy ! east face
-                strct(i,j)%AE   =   max(-mdot,0) + mu*dy/dx
-                IF (j==1) THEN !BC
-                    mdot            =   rho*strct(i  ,j+1)%v*dx ! north face
-                    strct(i,j)%AN   =   max(-mdot,0) + mu*dx*2./dy
-                ELSE
-                    mdot            =   rho*strct(i  ,j+1)%v*dx ! north face
-                    strct(i,j)%AN   =   max(-mdot,0) + mu*dx/dy
-                mdot            =   rho*strct(i-1,j  )%u*dy ! West face
-                strct(i,j)%AW   =   max( mdot,0) + mu*dy/dx
-                IF (j==max_x) THEN !BC
-                    mdot            =   rho*strct(i  ,j-1)%v*dx ! South face
-                    strct(i,j)%AS   =   max( mdot,0) + mu*dx*2./dy
-                ELSE
-                    mdot            =   rho*strct(i  ,j-1)%v*dy ! South face
-                    strct(i,j)%AS   =   max( mdot,0) + mu*dx/dy
-                strct(i,j)%AP   =   strct(i,j)%AE + &
-                                    strct(i,j)%AN + &
-                                    strct(i,j)%AW + &
-                                    strct(i,j)%AS
-            END DO
-        END DO
-
-        
-        DO i=2,max_x
-            DO j=1,max_y
-
-
-            END DO
-        END DO
-
-
-
-    END SUBROUTINE umom
-
-
-
-END MODULE types
 
 
 
@@ -107,13 +15,14 @@ PROGRAM project3
     USE types !use module defined by types
     IMPLICIT NONE
     ! declare variables
-    INTEGER ::  i,j,iter,k!,max_x=20,max_y=20
-    REAL    ::  dx,dy,error
+    INTEGER ::  i,j,iter!,max_x=20,max_y=20
+    REAL    ::  dx,dy
     REAL    ::  Lu,Ru,Tu,Bu ! boundary condition u velocity values
-    REAL    ::  u=0.,v=0.   ! velocity values
+    !REAL    ::  u=0.,v=0.   ! velocity values
     TYPE(dat),DIMENSION(0:max_xp,0:max_yp)::data ! 22 if you count edges (thin cell)
-    REAL    :: TIME1,TIME2  ! for time of computation
-    REAL(KIND=4),DIMENSION(2):: TIMEA  ! for time of computation
+    REAL    ::  TIME1,TIME2  ! for time of computation
+    !REAL(KIND=4),DIMENSION(2):: TIMEA  ! for time of computation
+    REAL    ::  error_RSS
     ! set dx and dy and gamma and coefficients (without dividing by delta x between node centers)
     dx=1./REAL(max_x)
     dy=1./REAL(max_y)
@@ -126,21 +35,31 @@ PROGRAM project3
     Tu = 1.
     Bu = 0.
     ! left Boundary
-    data(:,0)%u = Lu
+    data(0,:)%u     = Lu
     ! bottom boundary
-    data(0,:)%u = Bu
+    data(:,0)%u     = Bu
     ! right boundary
-    data(:,max_yp)%u = Ru
+    data(max_yp,:)%u= Ru
     ! top boundary
-    data(max_xp,:)%u = Tu
+    data(:,max_xp)%u= Tu
+    ! initialize u
+    data%u_old = data%u
+
 
     ! initialize v
-    data%v = 0.
+    data%v_old = 0.
+    data%v     = 0.
+    ! initialize P values
+    data%Pe=1.
+    data%Pn=1.
+    data%Pw=1.
+    data%Ps=1.
     ! point SOR method to solve for the exact values of phi using the BC (only loop through inner values)
     ! solving using the deferred correction method
     CALL CPU_TIME(TIME1)
+    DO iter=0,10000
     ! step 1 solve discretised momentum equations
-    CALL umom(data,dx,dy,max_xp,max_xp)
+    CALL mom_uv(data,dx,dy,max_x,max_x)
 
     ! step 2 Solve pressure correction equation
 
@@ -149,56 +68,40 @@ PROGRAM project3
     ! step 4 Solve all other discretised transport equations
 
     ! if no convergence, then iterate
+    error_RSS = 0.
+    DO i=1,max_x
+        DO j=1,max_y
+            error_RSS = error_RSS + (data(i,j)%u-data(i,j)%u_old)**2
+            error_RSS = error_RSS + (data(i,j)%v-data(i,j)%v_old)**2
+        END DO
+    END DO
+    error_RSS = sqrt(error_RSS)
+    ! reset values
+    data%u_old = data%u
+    data%v_old = data%v
+    !WRITE(*,*) "error = ",error_RSS
+
 
     ! if converged then stop
+    IF (error_RSS < 0.000001) THEN
+        EXIT
+    ELSE
+        WRITE(*,*) "iteration and error = ",iter,error_RSS
+    END IF
+END DO
 
 
-!    DO iter=0,100000000
-!        error = 0.
-!        DO j=1,max_x
-!            DO i=1,max_y
-!                    phi(i,j)%phi_new = (aw*phi(i,j-1)%phi_new + as*phi(i-1,j)%phi_new)/ap  &
-!                        -beta/ap * (&
-!                         apt*phi(i  ,j  )%phi_new &
-!                        -aet*phi(i  ,j+1)%phi_new &
-!                        -awt*phi(i  ,j-1)%phi_new &
-!                        -ant*phi(i+1,j  )%phi_new &
-!                        -ast*phi(i-1,j  )%phi_new &
-!                        -ap *phi(i  ,j  )%phi_new &
-!                        +aw *phi(i  ,j-1)%phi_new &
-!                        +as *phi(i-1,j  )%phi_new )
-!                IF (ISNAN(phi(i,j)%phi_new)) THEN
-!                    WRITE(*,*) "You really stink at this dude!  error on ",i,j," value of phi_new.  On iter = ",iter
-!                    WRITE(*,*) phi(i,j)%phi_new
-!                    STOP
-!                END IF
-!                error = error + (phi(i,j)%phi-phi(i,j)%phi_new)**2  ! RSS the error for each iteration
-!            END DO
-!        END DO
-!        ! BC ghost node values
-!        phi(max_yp,:)%phi_new = phi(max_y,:)%phi_new
-!        phi(:,max_xp)%phi_new = phi(:,max_x)%phi_new
-!        error = SQRT(error)                             ! sqrt to have RSS of error
-!        write(5,*) iter,error
-!        phi%phi = phi%phi_new                           ! set old phi to the new iteration guess
-!        IF (error .lt. 1.E-11) THEN
-!            WRITE(*,*) 'Did in ',iter,' iterations'
-!            WRITE(*,*) 'With RSS error = ',error
-!            EXIT                                        ! exit loop if error is small enough
-!        END IF
-!    END DO
     
-    CALL CPU_TIME(TIME2)
-    WRITE(*,*) "CPU Time = ",TIME2-TIME1
-    !output
-    ! user will need to specify size of 
-    !open(unit=9,file="output/x.txt");open(unit=10,file="output/y.txt")
-    !open(unit=11,file="output/phi.txt");
-    !100 FORMAT (max_x2p F14.6)
-    !WRITE( 9,100) ( phi(i,:)%x ,i=0,max_xp )
-    !WRITE(10,100) ( phi(i,:)%y ,i=0,max_xp )
-    !WRITE(11,100) ( phi(i,:)%phi ,i=0,max_xp )
-    !k=max_xp
-    !close(9);close(10);close(11);close(5)
-    !WRITE(*,*) "Wall Time = ",etime(TIMEA)
+CALL CPU_TIME(TIME2)
+WRITE(*,*) "CPU Time = ",TIME2-TIME1
+!output
+! user will need to specify size of 
+open(unit=9,file="output/x.txt")
+open(unit=10,file="output/y.txt")
+open(unit=11,file="output/u.txt")
+100 FORMAT (max_x2p F14.6)
+WRITE( 9,100) ( data(:,i)%x ,i=0,max_yp )
+WRITE(10,100) ( data(:,i)%y ,i=0,max_yp )
+WRITE(11,100) ( data(:,i)%u ,i=0,max_yp )
+close(9);close(10);close(11)
 END PROGRAM project3
---
+++ NS_staggered_grid/Project3_code/Types.f90
@@ -0,0 +1,122 @@
+MODULE types
+    !purpose: define data type struct
+    IMPLICIT NONE
+    TYPE::dat
+        REAL::x,y
+        REAL::u,v,u_old,v_old
+        REAL::AP,AE,AN,AS,AW
+        REAL::Pe,Pn,Ps,Pw
+        INTEGER::n
+    END TYPE dat
+CONTAINS
+    SUBROUTINE set_xy (strct,dx,dy,nx,ny)
+        REAL,INTENT(IN)     ::  dx,dy
+        INTEGER,INTENT(IN)  ::  nx,ny   ! size of strct in x and y directions 
+        TYPE(dat),DIMENSION(0:nx,0:ny),INTENT(INOUT)::strct ! data contained from 0:nx-1 where cells 0 and nx-1 are boundary nodes (cell volume approaches 0 on boundary nodes)
+        INTEGER ::  i,j,n   ! for do loops and n is counter for cell number
+        REAL    ::  xi,yi   ! x and y values for each cell
+        n=1                     ! cell number 1
+        DO i=1,ny-1             ! 1 to ny-2 for boundary nodes (we only are iterating through the middle values)
+            yi = i*dy - dy/2.   ! y coordinate
+            DO j=1,nx-1
+                xi = j*dx - dx/2.       ! x coordinate
+                strct(j,i)%n = n        ! input n node
+                strct(j,i)%x = xi       ! x coordinate to strct
+                strct(j,i)%y = yi       ! y coordinate to strct
+                !strct(i,j)%phi = 0.     ! phi value initialized guess
+                !strct(i,j)%phi_new = 0. ! phi value initialized guess for next iteration
+                n=n+1                   ! count cell numbers up one
+            END DO
+        END DO
+    ! left boundary
+    strct(0,:)%x = 0.
+    strct(0,0)%y = 0.
+    strct(0,1:nx-1)%y = RESHAPE((/ (i*dy - dy/2. ,i=1,nx-1) /),(/ nx-1/))
+    strct(0,nx+0)%y = 1.
+    ! bottom boundary
+    strct(0,0)%x = 0.
+    strct(1:ny-1,0)%x = RESHAPE((/ (i*dx - dx/2. ,i=1,ny-1) /),(/ ny-1/))
+    strct(ny+0,0)%x = 1.
+    strct(:,0)%y = 0.
+    ! right boundary
+    strct(ny+0,:)%x = 1.
+    strct(ny+0,:)%y = strct(0,:)%y
+    ! top boundary
+    strct(:,nx+0)%x = strct(:,0)%x
+    strct(:,nx+0)%y = 1.
+    END SUBROUTINE set_xy
+
+    SUBROUTINE mom_uv(strct,dx,dy,nx,ny)
+        ! requires uniform grid of dx and dy spacing
+        REAL,INTENT(IN)     ::  dx,dy
+        INTEGER,INTENT(IN)  ::  nx,ny   ! size of strct in x and y directions 
+        TYPE(dat),DIMENSION(0:nx+1,0:ny+1),INTENT(INOUT)::strct ! data contained from 0:nx+1 where cells 0 and nx+1 are boundary nodes (cell volume approaches 0 on boundary nodes)
+        REAL    ::  mu = 0.01
+        REAL    ::  rho= 1.
+        REAL    ::  mdot ! temporary value for mass flow values
+        INTEGER ::  i,j !loop iterators
+        REAL    ::  Omega = 0.5
+        DO i=2,nx
+            DO j=1,ny
+                mdot            =   rho*strct(i+1,j  )%u*dy ! east face
+                strct(i,j)%AE   =   max(-mdot,0.) + mu*dy/dx
+                IF (j==1) THEN !BC
+                    mdot            =   rho*strct(i  ,j+1)%v*dx ! north face
+                    strct(i,j)%AN   =   max(-mdot,0.) + mu*dx*2./dy
+                ELSE
+                    mdot            =   rho*strct(i  ,j+1)%v*dx ! north face
+                    strct(i,j)%AN   =   max(-mdot,0.) + mu*dx/dy
+                    mdot            =   rho*strct(i-1,j  )%u*dy ! West face
+                    strct(i,j)%AW   =   max( mdot,0.) + mu*dy/dx
+                END IF
+                IF (j==ny) THEN !BC
+                    mdot            =   rho*strct(i  ,j-1)%v*dx ! South face
+                    strct(i,j)%AS   =   max( mdot,0.) + mu*dx*2./dy
+                ELSE
+                    mdot            =   rho*strct(i  ,j-1)%v*dy ! South face
+                    strct(i,j)%AS   =   max( mdot,0.) + mu*dx/dy
+                END IF
+                strct(i,j)%AP   =   strct(i,j)%AE + &
+                                    strct(i,j)%AN + &
+                                    strct(i,j)%AW + &
+                                    strct(i,j)%AS
+            END DO
+        END DO
+
+        
+        DO i=2,nx
+            DO j=1,ny
+                strct(i,j)%u = (1.-Omega)*strct(i,j)%u_old &
+                                + &
+                                (Omega/strct(i,j)%AP) &
+                                * (&
+                                    strct(i,j)%AE*strct(i+1,j  )%u +&
+                                    strct(i,j)%AN*strct(i  ,j+1)%u +&
+                                    strct(i,j)%AW*strct(i-1,j  )%u +&
+                                    strct(i,j)%AS*strct(i  ,j-1)%u +&
+                                    (strct(i,j)%Pw-strct(i,j)%Pe)  *&
+                                    dx                              &
+                                )
+            END DO
+        END DO
+
+        DO i=1,nx
+            DO j=2,ny
+                strct(i,j)%v = (1.-Omega)*strct(i,j)%v_old &
+                                + &
+                                (Omega/strct(i,j)%AP) &
+                                * (&
+                                    strct(i,j)%AE*strct(i+1,j  )%v +&
+                                    strct(i,j)%AN*strct(i  ,j+1)%v +&
+                                    strct(i,j)%AW*strct(i-1,j  )%v +&
+                                    strct(i,j)%AS*strct(i  ,j-1)%v +&
+                                    (strct(i,j)%Ps-strct(i,j)%Pn)  *&
+                                    dy                              &
+                                )
+            END DO
+        END DO
+    END SUBROUTINE mom_uv
+
+
+
+END MODULE types
